2f85974145cec4374233878713d118a9164fadcb
Beulah Dorcas <beulah.dorcas@tolaram.com>
2025-11-21 02:19:07 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://poweroil-backend.onrender.com',
         changeOrigin: true
       }
     }
